b5788336b9126ecca31df692329fe953cedceb3b
Yur4-98 <47050612+Yur4-98@users.noreply.github.com>
2024-05-06 15:18:48 +0300

    feat: add registration and authorization forms

---
+++ src/Services/user.service.ts
@@ -0,0 +1,11 @@
+//import axios from 'axios';
+import { useState } from 'react';
+//import authHeader from './auth-header';
+
+const API_URL = 'http://localhost:3000/api/test/';
+
+class UserService {/*
+  */
+}
+
+export default new UserService();
