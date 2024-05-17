4a009a0d3c5c4854c777f65caf3bca108b13783f
Yur4-98 <47050612+Yur4-98@users.noreply.github.com>
2024-05-17 22:43:42 +0300

    Delete src directory

--- src/types/user.type.ts
+++
@@ -1,8 +0,0 @@
-export default interface IUser {
-    id?: any | null,
-    firstName?: string, 
-    lastName?: string, 
-    email?: string,
-    password?: string,
-    roles?: Array<string>
-  }
